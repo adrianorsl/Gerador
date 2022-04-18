d887bceabc62da669d6a2d1afdf618283d47d076
Adriano Zimmermann <adrianorslsc@hotmail.com>
2022-04-18 20:07:32 -0300

    Com css sem grafico

--- gerador.php
+++ gerador.php
@@ -6,7 +6,8 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Gerador</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <form action="" method="post">
@@ -21,15 +22,15 @@
         $imp = 0;
        
         $soma = array_sum($json);
-        echo "<h1 classe='soma'> Soma dos números: $soma <br>";
+        echo "<h1 class='soma'> Soma dos números: $soma <br>";
         $ordenar[] = sort($json) ;
         $t = count($json);
         $menor = $json[0]; 
-        echo "<h1 classe='soma' > O menor número é: $menor <br>";
+        echo "<h1 class='soma' > O menor número é: $menor <br>";
         $maior = $json[$t - 1];
-        echo "<h1 classe='soma'> O maior número é: $maior <br>";
+        echo "<h1 class='soma'> O maior número é: $maior <br>";
         $media = $soma / count($json);
-        echo "<h1 clase='soma'> A media dos números é: $media <br>";
+        echo "<h1 class='soma'> A media dos números é: $media <br>";
         for($i = 0; $i < count($json); $i++){
             if ($json[$i] % 2 == 0){
                 $pares[$x] = $json[$i];
@@ -40,11 +41,11 @@
             }
 
         }
-        echo "<h1 classe='pares'> São pares <br>";
+        echo "<h1 class='pares'> São pares <br>";
         for ($i = 0; $i < count($pares); $i++){
             echo $pares[$i]."<br>";
         }
-        echo "<h1 classe='impares'> São impares <br>";
+        echo "<h1 class='impares'> São impares <br>";
         for ($i = 0; $i < count($impares); $i++){
             echo $impares[$i]."<br>";
         }
@@ -72,32 +73,32 @@
                 }
             }
         }
-        echo "<h1 classe='media'> Maiores que a media <br>";
+        echo "<h1 class='media'> Maiores que a media <br>";
         for ($i = 0; $i < count($maiorMedia); $i++){
             echo $maiorMedia[$i]."<br>";
         }
-        echo "<h1 classe='media'> Menores que a media <br>";
+        echo "<h1 class='media'> Menores que a media <br>";
         for ($i = 0; $i < count($menorMedia); $i++){
             echo $menorMedia[$i]."<br>";
         }
         
         if ($primo != null){
-            echo "<h1 classe='media'> São primos <br>";
+            echo "<h1 class='media'> São primos <br>";
             for ($i = 0; $i < count($primo); $i++){
                 echo $primo[$i]."<br>";
             }
         }else{
-            echo "<h1 classe='media'> Não há números primos <br>";
+            echo "<h1 class='media'> Não há números primos <br>";
         }
         
         if (count($json) % 2 == 0){
             $u = count($json) / 2;
             $mediana = ($json[$u] + $json[$u+1]) / 2;
-            "<h1 clase='media'> A mediana é: $mediana <br>";
+            "<h1 class='media'> A mediana é: $mediana <br>";
         }else{
             $u = (int)(count($json) / 2);
             $mediana = $json[$u];
-            echo "<h1 clase='media'> A mediana é: $mediana <br>";
+            echo "<h1 class='media'> A mediana é: $mediana <br>";
         } 
        
 
